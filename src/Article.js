0c4b11d2f2d5e99ad558752aecebec0bbb1cdec5
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-09-27 22:45:35 +0300

    add toggleOpen

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+export default class Article extends Component {
+
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        const { article } = this.props
+        const { isOpen } = this.state
+
+        const body = isOpen ? <section>{article.text}</section> : null
+        return (
+            <div>
+                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
+                {body}
+            </div>
+        )
+    }
+
+    toggleOpen = ev => {
+        console.log('---', ev)
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+/*
 export default (props) => {
     const { article } = props
     return (
@@ -8,5 +36,4 @@ export default (props) => {
             <section>{article.text}</section>
         </div>
     )
-
-}
+}*/

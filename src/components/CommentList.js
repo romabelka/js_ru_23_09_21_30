bac867a1d27b6bc7809f21d65db4184445d6907e
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-07 18:47:53 +0300

    add NewCommentForm

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,14 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 import Comment from './Comment'
 import toggleOpen from './../decorators/toggleOpen'
+import NewCommentForm from './NewCommentForm'
 
 function CommentList(props) {
     const { comments, isOpen, toggleOpen } = props
-    if (!comments || !comments.length) return <p>No comments yet</p>
+    if (!comments || !comments.length) return <div><p>No comments yet</p><NewCommentForm /></div>
 
     const commentItems = comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)
     const text = isOpen ? 'hide comments' : `show ${comments.length} comments`
-    const body = isOpen && <ul>{commentItems}</ul>
+    const body = isOpen && <div><ul>{commentItems}</ul><NewCommentForm /></div>
 
     return (
         <div>
---
+++ src/components/NewCommentForm.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react'
+
+class NewCommentForm extends Component {
+    static propTypes = {
+    };
+
+    state = {
+        text: '',
+        user: ''
+    }
+
+    handleChange = field => ev => this.setState({
+        [field]: ev.target.value
+    })
+
+    handleSubmit = ev => {
+        ev.preventDefault()
+        console.log('---', this.state)
+        this.setState({
+            user: '',
+            text: ''
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit = {this.handleSubmit}>
+                comment: <input type="text" value={this.state.text} onChange = {this.handleChange('text')}/>
+                comment: <input type="text" value={this.state.user} onChange = {this.handleChange('user')}/>
+                <input type = "submit"/>
+            </form>
+        )
+    }
+}
+
+export default NewCommentForm

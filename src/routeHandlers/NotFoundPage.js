fc30cbc8f0d71bb8c9ffe044939b63d12628a862
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-21 22:46:02 +0300

    add NotFoundPage

---
+++ src/routeHandlers/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+
+function NotFoundPage(props) {
+    return (
+        <div>
+            <h3>Page not found</h3>
+        </div>
+    )
+}
+
+NotFoundPage.propTypes = {
+}
+
+export default NotFoundPage
--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,16 @@ import NewArticleForm from './components/NewArticleForm'
 import Filters from './components/Filters'
 import Counter from './components/Counter'
 import ArticlePage from './routeHandlers/ArticlePage'
+import NotFoundPage from './routeHandlers/NotFoundPage'
 
 export default <Router history = {browserHistory}>
     <Route path = "/" component = {Container} >
         <Route path = "articles" component = {ArticleList}>
+            <Route path = "new" component = {NewArticleForm} />
             <Route path = ":id" component = {ArticlePage}/>
         </Route>
         <Route path = "filters" component = {Filters} />
         <Route path = "counter" component = {Counter} />
-        <Route path = "new" component = {NewArticleForm} />
+        <Route path = "*" component = {NotFoundPage} />
     </Route>
 </Router>

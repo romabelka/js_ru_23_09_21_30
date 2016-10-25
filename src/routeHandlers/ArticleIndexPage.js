d48ec6e5b2ad33be7b992179fa96ca3fb3c07d05
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-25 21:43:48 +0300

    add index route and redirects

---
+++ src/routeHandlers/ArticleIndexPage.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+
+function ArticleIndexPage(props) {
+    return (
+        <div>
+            <h1>Please select article</h1>
+        </div>
+    )
+}
+
+ArticleIndexPage.propTypes = {
+}
+
+export default ArticleIndexPage
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, Redirect, IndexRedirect, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import Container from './components/Container'
 import ArticleList from './routeHandlers/ArticleListPage'
 import NewArticleForm from './components/NewArticleForm'
@@ -9,15 +9,20 @@ import ArticlePage from './routeHandlers/ArticlePage'
 import NotFoundPage from './routeHandlers/NotFoundPage'
 import CommentsRoot from './routeHandlers/CommentsRoot'
 import CommentsPage from './routeHandlers/CommentsPage'
+import ArticleIndexPage from './routeHandlers/ArticleIndexPage'
 
 export default <Router history = {browserHistory}>
     <Route path = "/" component = {Container} >
+        <IndexRedirect to = "/articles" />
+        <Redirect from = "article" to = "articles" />
         <Route path = "articles" component = {ArticleList}>
+            <IndexRoute component = {ArticleIndexPage} />
             <Route path = "new" component = {NewArticleForm} />
             <Route path = ":id" component = {ArticlePage}/>
         </Route>
         <Route path = "filters" component = {Filters} />
         <Route path = "comments" component = {CommentsRoot}>
+            <IndexRedirect to = "1" />
             <Route path = ":page" component = {CommentsPage} />
         </Route>
         <Route path = "*" component = {NotFoundPage} />
